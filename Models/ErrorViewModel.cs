04112e8792641aa7a978fb31f6bb2470b66147f0
AnthonyCollel17 <ST10454956@rcconnect.edu.za>
2025-10-24 18:15:43 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CMCS_POE_PART_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
