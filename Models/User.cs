70d44eee2921cdb32f8dd2d3a8c0ca9c8b0bfffd
AnthonyCollel17 <ST10454956@rcconnect.edu.za>
2025-10-24 18:57:25 +0200

    Coded ser models for the database

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,14 @@
-﻿namespace CMCS_POE_PART_2.Models
+﻿namespace CMCS.Models
 {
     public class User
     {
+        public int userID { get; set; }
+        public string full_names { get; set; } = string.Empty;
+        public string surname { get; set; } = string.Empty;
+        public string email { get; set; } = string.Empty;
+        public string role { get; set; } = string.Empty;
+        public string gender { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+        public DateTime date { get; set; } = DateTime.Today;
     }
 }
