38ed2d842913902b342c494cd02a46b34d53029a
AnthonyCollel17 <ST10454956@rcconnect.edu.za>
2025-10-24 19:17:34 +0200

    Eddited the program file to work well with my code

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
+using CMCS_POE_PART_2.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,10 +23,16 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
+
+// Init DB on startup
+var dbInit = new auto_create_instance_db_tables();
+dbInit.InitializeSystemAsync();
 
 app.Run();
