9aa86de9be1027f6a3b53d106c946ec1c8641740
AnthonyCollel17 <ST10454956@rcconnect.edu.za>
2025-11-19 20:02:45 +0200

    minor touches to the code to look cleaner

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace CMCS_POE_PART_2.Controllers
             var role = HttpContext.Session.GetString("Role");
             if (string.IsNullOrEmpty(role))
                 return RedirectToAction("Login", "Account");
+
             ViewBag.Role = role;
             ViewBag.UserName = HttpContext.Session.GetString("UserName");
             return View();
